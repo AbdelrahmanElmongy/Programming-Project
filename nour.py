5d0d7f5a6ced12db81557182dc329739c8a582ca
AbdelrahmanElmongy <121382128+AbdelrahmanElmongy@users.noreply.github.com>
2024-03-07 22:38:59 +0200

    Add files via upload

---
+++ nour.py
@@ -0,0 +1,62 @@
+# The Area Calculating
+pi=3.14
+r=float(input('please enter your r'))
+area=pi*r*r
+print("The Area Is")
+print(area)
+
+#The Preimeter Calculating
+pi=3.14
+r=float(input('please enter your r'))
+Preimeter=2*r*pi
+print("The Preimeter Is")
+print(Preimeter)
+
+#The square Area
+l=float(input('please enter your l'))
+Area=l*l
+print("The Area")
+print(Area)
+
+#The square Circumference
+circumference=l*4
+print("The Circumference")
+print(circumference)
+
+#The rectangle Area
+x=float(input('please enter your x'))
+y=float(input('please enter your y'))
+Area=x*y
+print("The Area")
+print(Area)
+
+#The rectangle Preimeter
+perimeter=(x+y)*2
+print("The Perimeter")
+print(perimeter)
+
+#The cylinder Volume
+m=float(input('please enter your m'))
+r=float(input('please enter your r'))
+n=pi*r*r
+Volume=n*m
+print("The Volume")
+print(Volume)
+
+#The rectengular prism Area
+l=float(input('please enter your l'))
+k=float(input('please enter your k'))
+m=float(input('please enter your m'))
+Area=((l*k)*2)+((l*m)*2)+((k*m)*2)
+print("The Area")
+print(Area)
+
+#The  cube Volume
+l=float(input('please enter your l'))
+volume=l*l*l
+print("The Volume")
+print(Volume)
+
+
+
+
